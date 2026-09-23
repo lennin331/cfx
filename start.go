563f65000be4a203ce67f604de4e782b3dc0c291
lennin331 <lenin12906@gmail.com>
2026-09-23 10:01:11 +0530

    fetch: title, time and space with 4 calls in total

--- start.go
+++ start.go
@@ -18,7 +18,8 @@ import(
 	"fmt"
 	//"os"
 	"time"
-	"strings"
+	//"strings"
+	"regexp"
 	"github.com/gocolly/colly/v2"
 )
 type problem struct {
@@ -40,17 +41,27 @@ func main(){
 		colly.Async(true),
 	)
 	c.OnHTML(".problem-statement", func(e *colly.HTMLElement){
-	fetched_problem.title = e.ChildText(".header .title")
-  // fetched_problem.time = e.ChildText("*> .header .time-limit") 
-  // fetched_problem.space = e.ChildText("* >.header .memory-limit")
-	fmt.Println(fetched_problem.time)
-	c.OnHTML(".header .time-limit", func(el *colly.HTMLElement){
-		// time-limit has property class that has "property-title" that "time limit per test"
-		// just ugly formatting trying to format without the grand child element 
-		clone := el.DOM.Clone()
-		clone.Children().Remove()
-		fetched_problem.time = strings.TrimSpace(clone.Text())
-	})
+	fetched_problem.title = e.ChildText(".header .title") // title - variable 
+
+  fetched_problem.space = e.ChildText(".header .memory-limit .property-title") 
+  fetched_problem.time = e.ChildText(".header .time-limit .property-title") 
+
+	memory_value:= e.ChildText(".header .memory-limit ")
+	time_value := e.ChildText(".header .time-limit ")
+	//regex to get only the number for the time_value - varuable
+	reg := regexp.MustCompile(`[^\d.]`)
+	time_value = reg.ReplaceAllString(time_value, "")
+	memory_value = reg.ReplaceAllString(memory_value, "")
+
+
+
+
+
+
+	//printables are kept seperately 
+	fmt.Println(fetched_problem.title)
+	fmt.Println(fetched_problem.time + ": " + time_value + "s")
+	fmt.Println(fetched_problem.space + ": " + memory_value + "Mb")
 	})
 	c.Visit("https://codeforces.com/problemset/problem/2264/D")
   c.Wait()
