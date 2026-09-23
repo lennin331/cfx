0dee7344d66ca55e459cf163d9579cb637bb2047
lennin331 <lenin12906@gmail.com>
2026-09-23 13:34:10 +0530

    fetch: formatted question text fetched

--- start.go
+++ start.go
@@ -1,3 +1,13 @@
+package main
+import(
+	"fmt"
+	//"os"
+	"time"
+	"strings"
+	"regexp"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly/v2"
+)
 /*
 format:
 	title
@@ -12,24 +22,14 @@ format:
 	output 
 	testcase explaination (might inclued images)
 */
-package main
-
-import(
-	"fmt"
-	//"os"
-	"time"
-	//"strings"
-	"regexp"
-	"github.com/gocolly/colly/v2"
-)
 type problem struct {
 	requeste_at time.Time //time of request for the record V^_^V
 	title string
 	time string
 	space string
-	problem_header string //handle for images
-  problem_body string //handle here too 
-	problem_footer string //and here
+	problem_text string //handle for images
+  problem_input_specification string //handle here too 
+	problem_output_specification string //and here
 	input string 
 	output string
 	sample_tests string
@@ -53,15 +53,25 @@ func main(){
 	time_value = reg.ReplaceAllString(time_value, "")
 	memory_value = reg.ReplaceAllString(memory_value, "")
 
-
-
-
-
+	
+e.DOM.Find(".header ~ div").Each(func(_ int, s * goquery.Selection){
+		if s.HasClass("input-specification") || s.HasClass("output-specification") || s.HasClass("sample-tests"){
+			return
+		}
+		s.Find("p").Each(func(_ int, p* goquery.Selection){
+			text:= strings.TrimSpace(p.Text())
+			if text != "" {
+				fetched_problem.problem_text += text +"\n"
+			}
+		})
+		fetched_problem.problem_text += "\n\n" // this apparently handles the note part at the bottom of the page
+	})
 
 	//printables are kept seperately 
 	fmt.Println(fetched_problem.title)
 	fmt.Println(fetched_problem.time + ": " + time_value + "s")
 	fmt.Println(fetched_problem.space + ": " + memory_value + "Mb")
+	fmt.Println(fetched_problem.problem_text)
 	})
 	c.Visit("https://codeforces.com/problemset/problem/2264/D")
   c.Wait()
